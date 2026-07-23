7c392c18fcef225c8afa2f971a9c4721b9c3e8d8
Stardust <miguel.ramos9191@gmail.com>
2026-07-23 16:40:19 -0500

    bug fixe

--- RPG/CombatSystem.cpp
+++ RPG/CombatSystem.cpp
@@ -5,61 +5,40 @@
 #include "EnemyStats.h"
 
 int playerAttack(PlayerStats& player, int enemyDefense) {
-
-        int damage = (player.PlayerAttack * 100) / (100 + enemyDefense);
-
+    int damage = (player.PlayerAttack * 100) / (100 + enemyDefense);
+    
     if (damage < 1) {
-
         damage = 1;
-
     }
-
+    
     return damage;
 }
 
 int playerDefend(PlayerStats& player, int enemyDamage) {
-
-<<<<<<< Updated upstream
-    float reducedDamage =  enemyDamage - (enemyDamage * player.PlayerDefense / 100);;
-=======
-    float reduction = player.PlayerDefense / (player.PlayerDefense / 100.f);
+    float reducedDamage = enemyDamage - (enemyDamage * player.PlayerDefense / 100.0f);
     
-    int reducedDamage = enemyDamage * 1.0f - reduction;
->>>>>>> Stashed changes
-
     if (reducedDamage < 1) {
-        
         reducedDamage = 1;
-
     }
-
-    return reducedDamage;
-
+    
+    return static_cast<int>(reducedDamage);
 }
 
-
 void playerHeal(PlayerStats& player) {
-
     const int HEAL_AMOUNT = 30;
     const int maxHealth = 100;
     
     if (player.PlayerHealth == maxHealth) {
-
         std::cout << "Player cannot heal - already at max health!" << std::endl;
         return;
-
     }
     
     int newHealth = player.PlayerHealth + HEAL_AMOUNT;
-
+    
     if (newHealth > maxHealth) {
-
         newHealth = maxHealth;
-
     }
     
-
-    player.PlayerHealth = newHealth;  
-
+    player.PlayerHealth = newHealth;
     std::cout << "Player healed to " << player.PlayerHealth << " health!" << std::endl;
 }
